5778849c89633a19faaa7fe6d9c2b264ace26d3d
k1b1t0 <153260776+k1b1t0@users.noreply.github.com>
2026-01-07 11:16:43 +0700

    Add HTML Base plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,9 @@
+const { HtmlBasePlugin } = require("@11ty/eleventy");
+
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPlugin(HtmlBasePlugin);
   return {
     pathPrefix: "/ducnm-blog/", // Đảm bảo trùng với tên repo
     dir: {
