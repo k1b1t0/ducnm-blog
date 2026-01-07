a10033b937bb489c795dcf18586162a59ccf57c6
k1b1t0 <153260776+k1b1t0@users.noreply.github.com>
2026-01-07 11:11:56 +0700

    Add prefix in eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,11 @@
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("assets");
+  return {
+    pathPrefix: "/ducnm-blog/", // Đảm bảo trùng với tên repo
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
 };
